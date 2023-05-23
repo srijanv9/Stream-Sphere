083b49163013f62e42883b18ad0f5a06732597bc
Srijan Verma <srijanv9@gmail.com>
2023-05-23 14:13:18 +0530

    api issue resolved

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,11 +1,6 @@
-const GOOGLE_API = "AIzaSyBH06OJtMWkc71z8Y0ipVz5yCNmCgaVksU";
-export const YOUTUBE_VIDEOS_API =
-  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
-  GOOGLE_API;
+console.log("this is my api-->", process.env.REACT_APP_GOOGLE_API_KEY);
 
-console.log("-->", process.env.REACT_APP_GOOGLE_API_KEY);
-
-// export const YOUTUBE_VIDEOS_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="${process.env.REACT_APP_GOOGLE_API_KEY}`;
+export const YOUTUBE_VIDEOS_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${process.env.REACT_APP_GOOGLE_API_KEY}`;
 // const GOOGLE_API2 = {process.env.REACT_APP_GOOGLE_API_KEY};
 // console.log("this is ok :", GOOGLE_API2);
 export const YOUTUBE_SEARCH_API =
